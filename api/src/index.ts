672f74437c3fc173b20cda8fc044959a45f71fd5
Kjartan <Kjartan.Einarsson@quartech.com>
2023-05-11 13:58:44 -0700

    add express basic setup to api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,33 @@
-console.log("hello world");
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+// defining the Express app
+const app = express();
+
+// defining an array to work as the database (temporary solution)
+const ads = [{ title: 'Hello, world (again)!' }];
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+// defining an endpoint to return all ads
+app.get('/', (req, res) => {
+  res.send(ads);
+});
+
+// starting the server
+app.listen(3001, () => {
+  console.log('listening on port 3001');
+});
