2f4eb29ada29588540e22fbdde24405b47e7f4e0
mmojtaba <44787178+MMojtaba@users.noreply.github.com>
2023-06-05 19:58:28 -0700

    I think codecov with mocha working locally

--- api/src/tmp.test.js
+++ api/src/tmp.test.js
@@ -1,3 +1,13 @@
-function someTest() {
-    return 1;
-}
+// function someTest() {
+//     return 1;
+// }
+
+
+var assert = require('assert');
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
