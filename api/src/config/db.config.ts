3826183a4adfbb6fc900d2a96a823e9e426dbd18
Kjartan <Kjartan.Einarsson@quartech.com>
2023-06-22 10:27:04 -0700

    fix merge conflict, add comments, clean code

--- api/src/config/db.config.ts
+++ api/src/config/db.config.ts
@@ -2,10 +2,9 @@ const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = Number(process.env.DB_PORT);
 const DB_USERNAME = process.env.DB_ADMIN;
 const DB_PASSWORD = process.env.DB_ADMIN_PASS;
-const MONGO_URI = process.env.MONGO_URI;
 
 export default {
   database: {
-    url: MONGO_URI ? MONGO_URI : `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`
+    url: `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`
   }
 };
