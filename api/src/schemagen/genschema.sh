c336461154ae44ec078b78febbb23154945e6f03
Gavin <gavinfreeborn@gmail.com>
2023-07-25 14:50:13 -0700

    document change to genschema.sh

--- api/src/schemagen/genschema.sh
+++ api/src/schemagen/genschema.sh
@@ -3,6 +3,7 @@
 # requires openapi2jsonschema via `pip install openapi2jsonschema`
 
 openapi2jsonschema --stand-alone ../../docs/api_schema.json
+# remove the files that cause reference/lookup errors due to infinite recursion
 rm schemas/error.json
 rm schemas/all.json
 npx json2ts -i ./schemas/ -o types/
