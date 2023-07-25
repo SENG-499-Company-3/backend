f3d0e941b817f0ec9c0dc7465e664133a205479f
Gavin <gavinfreeborn@gmail.com>
2023-07-25 15:39:49 -0700

    idk how i missed this

--- api/src/routes/preferences.routes.ts
+++ api/src/routes/preferences.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { TeacherPrefController } from '../controllers/teacherpref.controller';
 import { isAdmin, getName, getUid } from '../helpers/auth';
-import { validateEndpoint } from 'express-jsonschema';
+import { validate as validateEndpoint } from 'express-jsonschema';
 import { validate } from 'jsonschema';
 
 import preference from '../schemagen/schemas/preference.json';
@@ -33,62 +33,61 @@ const teacherPrefController: TeacherPrefController = new TeacherPrefController()
 //   }
 //
 //   try {
-//     const preference_update = undefined;
-//     await courseController.update(preference_update);
-//     res.status(200).send(validate({ message: 'Updated course.' }, preference));
+//     const preference : Preference = await teacherPrefController.byId(params.teacher_id);
+//     res.status(200).send(validate(preference, preference));
 //   } catch (err) {
 //     res.status(401).send({ message: err });
 //   }
 // };
 // router.get('/:teacher_id', get_teacher_preferences);
-
-const update_teacher_preferences = async (
-  { headers, body }: { headers: any; body: Preference },
-  res: any
-): Promise<void> => {
-  if (!headers.authorization) {
-    res.status(401).send({ message: 'This endpoint requires authorization header.' });
-    return;
-  }
-  const authToken = headers.authorization;
-  const isAdm = await isAdmin(authToken);
-  if (!isAdm) {
-    res.status(401).send({ message: 'Need admin access.' });
-    return;
-  }
-
-  try {
-    // await teacherPrefController.update(body. );
-    res.status(200).send(validate({ message: 'Updated course.' }, preference));
-  } catch (err) {
-    res.status(401).send({ message: err });
-  }
-};
-router.put('/', validateEndpoint({ body: preference }), update_teacher_preferences);
-
-// const get_teacher_preferences = async ({headers}: {headers: any}, res: any): Promise<void> => {
-//     if(!headers.authorization)
-//     {
-//         res.status(401).send({ message: "This endpoint requires authorization header."});
-//         return;
-//     }
-//     const authToken = headers.authorization;
-//     const isAdm = await isAdmin(authToken);
-//     if(!isAdm)
-//     {
-//         res.status(401).send({message: "Need admin access."});
-//         return;
-//     }
 //
-//     try
-//     {
-//         const preference_update = undefined;
-//         await courseController.update(preference_update);
-//         res.status(200).send(validate({message: "Updated course."}, preference));
-//     } catch (err)
-//     {
-//         res.status(401).send({message: err});
-//     }
+// const update_teacher_preferences = async (
+//   { headers, body }: { headers: any; body: Preference },
+//   res: any
+// ): Promise<void> => {
+//   if (!headers.authorization) {
+//     res.status(401).send({ message: 'This endpoint requires authorization header.' });
+//     return;
+//   }
+//   const authToken = headers.authorization;
+//   const isAdm = await isAdmin(authToken);
+//   if (!isAdm) {
+//     res.status(401).send({ message: 'Need admin access.' });
+//     return;
+//   }
+//
+//   try {
+//     await teacherPrefController.update(body. );
+//     res.status(200).send(validate({ message: 'Updated course.' }, preference));
+//   } catch (err) {
+//     res.status(401).send({ message: err });
+//   }
+// };
+// router.put('/', validateEndpoint({ body: preference }), update_teacher_preferences);
+//
+// const get_my_teacher_preferences = async ({headers}: {headers: any}, res: any): Promise<void> => {
+//   if(!headers.authorization)
+//   {
+//     res.status(401).send({ message: "This endpoint requires authorization header."});
+//     return;
+//   }
+//   const authToken = headers.authorization;
+//   const isAdm = await isAdmin(authToken);
+//   if(!isAdm)
+//   {
+//     res.status(401).send({message: "Need admin access."});
+//     return;
+//   }
+//
+//   try
+//   {
+//
+//     const preferences: Preference = await teacherPrefController.byId(undefined)
+//     res.status(200).send(validate(preferences, preference));
+//   } catch (err)
+//   {
+//     res.status(401).send({message: err});
+//   }
 // }
-// router.get('/', get_teacher_preferences);
+// router.get('/', get_my_teacher_preferences);
 // module.exports = router;
